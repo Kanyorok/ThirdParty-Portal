bc8b9d4918814ed6743d8c41a4d32ba42833f98e
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-04-11 11:10:13 +0300

    Add linters for checks on best practices

--- app/Models/ItemCategory.php
+++ app/Models/ItemCategory.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemCategory extends Model
 {
-    //
+    protected $table = 't_items_categories';
+    protected $fillable = ['name',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class, 'category_id');
+    }
 }
