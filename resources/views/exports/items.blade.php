6ced8acff8049d387695146b48c664546241a56f
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-04-16 10:43:23 +0300

    Fix the fields display of records

--- resources/views/exports/items.blade.php
+++ resources/views/exports/items.blade.php
@@ -1,19 +1,27 @@
 <table>
     <thead>
         <tr>
+            <th>Serial</th>
+            <th>Item Code</th>
             <th>Name</th>
             <th>Type</th>
             <th>Category</th>
+            <th>Description</th>
             <th>Unit Price</th>
+            <th>UOM</th>
         </tr>
     </thead>
     <tbody>
         @foreach($items as $item)
             <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $item->UniqueCode }}</td>
                 <td>{{ $item->Name }}</td>
                 <td>{{ ucfirst($item->Type) }}</td>
                 <td>{{ optional($item->category)->Name }}</td>
-                <td>{{ $item->UnitPrice }}</td>
+                <td>{{ $item->Description }}</td>
+                <td>{{ $item->Currency }} {{ $item->UnitPrice }}</td>
+                <td>{{ $item->UOM }}</td>
             </tr>
         @endforeach
     </tbody>
