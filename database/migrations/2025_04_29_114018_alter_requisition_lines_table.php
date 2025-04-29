0947af3230a793a73b5c281e27db2b30c2c6bec4
Eric Muroki <Murokieric4@gmail.com>
2025-04-29 13:49:10 +0300

    Requisition Header Merge

---
+++ database/migrations/2025_04_29_114018_alter_requisition_lines_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('t_RequisitionLines', function (Blueprint $table) {
+//            $table->dropColumn(['Module', 'ExpectedPrice']);
+            $table->dropColumn('NeededBy');
+            $table->date('NeededBy')->nullable();
+            $table->decimal('ExpectedPrice')->nullable();
+            $table->unsignedBigInteger('RequisitionID')->nullable()->change(); // make sure it's nullable
+            $table->foreign('RequisitionID')->references('Id')->on('t_Requisitions')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('t_RequisitionLines', function (Blueprint $table) {
+//            $table->string('Module')->nullable();
+//            $table->decimal('ExpectedPrice', 15, 2)->nullable();
+            $table->dropForeign(['RequisitionID']);
+            $table->dropColumn('NeededBy');
+        });
+    }
+};
