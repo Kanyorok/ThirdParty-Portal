75fe8fe303e4e10e3f8bf869a3d6ed0d08a44996
unknown <rmjesusaddict@gmail.com>
2025-05-01 05:06:22 +0300

    Align the migrations for a fresh installation

--- database/migrations/2025_04_29_114018_alter_requisition_lines_table.php
+++ database/migrations/2025_04_29_114018_alter_requisition_lines_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('t_RequisitionLines', function (Blueprint $table) {
 //            $table->dropColumn(['Module', 'ExpectedPrice']);           
             $table->date('NeededBy')->nullable();
-            $table->decimal('ExpectedPrice')->nullable();
             $table->unsignedBigInteger('RequisitionID')->nullable()->change(); // make sure it's nullable
             $table->foreign('RequisitionID')->references('Id')->on('t_Requisitions')
                 ->onDelete('set null');
