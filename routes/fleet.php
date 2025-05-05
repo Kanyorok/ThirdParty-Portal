3742e4bf36ff6bc3fd009ae4f7794037019d8f8e
Joseph Njoroge <Joseph.Njoroge@CRAFTSILICON.LOCAL>
2025-05-05 14:32:24 +0300

    Fix the route structure to avoid conflict

--- routes/fleet.php
+++ routes/fleet.php
@@ -2,11 +2,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Fleetmanagement\TripManagementController;
 use App\Http\Controllers\Fleetmanagement\FuelManagementController;
-
+use App\Http\Controllers\Fleetmanagement\DriverManagementController;
 
 Route::namespace('Fleetmanagement')->group(function () {
     Route::resource('tripmanagement', TripManagementController::class);
     Route::resource('fuelmanagement', FuelManagementController::class);
-
+    Route::resource('drivermanagement', DriverManagementController::class);
 
 });
--- routes/fleetmanagement.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Fleetmanagement\DriverManagementController;
-
-
-Route::namespace('Fleetmanagement')->group(function () {
-    Route::resource('drivermanagement', DriverManagementController::class);
-});
