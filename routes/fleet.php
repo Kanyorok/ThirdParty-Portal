bf542ebd6e460fdd60ea05d96c50d2b8b73400d0
Felix Chege <Felix.Chege@CRAFTSILICON.LOCAL>
2025-05-07 16:25:29 +0300

    Add menu items to the fleet management

--- routes/fleet.php
+++ routes/fleet.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Fleetmanagement\DriverManagementController;
 use App\Http\Controllers\Fleetmanagement\ServiceTrackingController;
 use App\Http\Controllers\Fleetmanagement\LicensingController;
 use App\Http\Controllers\Fleetmanagement\VehicleManagementController;
+use App\Http\Controllers\Fleetmanagement\ComplianceAndDocumentationController;
+use App\Http\Controllers\Fleetmanagement\FleetProcurementAndDisposalController;
+use App\Http\Controllers\Fleetmanagement\InventoryOfSparePartsController;
 
 Route::namespace('Fleetmanagement')->group(function () {
     Route::resource('tripmanagement', TripManagementController::class);
@@ -14,5 +17,8 @@ Route::namespace('Fleetmanagement')->group(function () {
     Route::resource('servicetracking', ServiceTrackingController::class);
     Route::resource('licensing', LicensingController::class);
     Route::resource('vehicle-registry', VehicleManagementController::class);
-   
+    Route::resource('complianceanddocumentation', ComplianceAndDocumentationController::class);
+    Route::resource('fleetprocurementanddisposal', FleetProcurementAndDisposalController::class);
+    Route::resource('inventoryofspareparts', InventoryOfSparePartsController::class);
+    
 });
