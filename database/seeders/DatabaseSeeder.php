f124d20304a93eb254fad27e1d1e7a3a7453993b
unknown <rmjesusaddict@gmail.com>
2025-06-01 07:32:37 +0300

    Fix all errors noted in the code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,8 @@ class DatabaseSeeder extends Seeder
         $this->call(ItemCategoriesSeeder::class);
         $this->call(ItemTypeSeeder::class);
         $this->call(InventoryTypeSeeder::class);
+        $this->call(UnitOfMeasureSeeder::class);
         $this->call(ItemMasterListSeeder::class);
+        $this->call(DepartmentNeedsSeeder::class);
     }
 }
