d20fafd3edf612b0a08d8f193319ce80cd80ec7c
Daniel Mbugua <Daniel.Mbugua@CRAFTSILICON.LOCAL>
2025-06-10 17:35:37 +0300

    Fixed Module Seeder

--- app/Models/Budget/BudgetPeriodTypes.php
+++ app/Models/Budget/BudgetPeriodTypes.php
@@ -10,9 +10,17 @@ class BudgetPeriodTypes extends Model
 {
      use UserActorTrait,SoftDeletes;
 
+    protected $primaryKey = 'Id';
+    
     const CREATED_AT = 'CreatedOn';
     const UPDATED_AT = 'ModifiedOn';
     const DELETED_AT = 'DeletedOn';
+
+    public static function getPrimaryKey(): string
+    {
+        return 'BudgetPeriodTypesId';
+    }
+
 
     protected $table = 't_BudgetPeriodTypes';
 
