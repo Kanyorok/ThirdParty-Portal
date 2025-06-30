d1d337f636274506f0d4f419c5e93e424eca5e7f
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-06-30 18:22:35 +0300

    Add the model

--- app/Models/Auth/ModelRole.php
+++ app/Models/Auth/ModelRole.php
@@ -10,7 +10,8 @@ class ModelRole extends Model
 {
     protected $table = 't_ModelRoles';
     protected $fillable = ['model_id', 'model_type', 'role_id', 'BranchId'];
-
+    public $timestamps = false;
+    
     public function branch()
     {
         return $this->belongsTo(Branch::class, 'BranchId');
