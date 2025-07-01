3a2835ff5cc9f7bb3710902ea440c254423050a9
Mureithi Maina <mureithi@razorinformatics.co.ke>
2025-07-01 14:34:44 +0300

    feat: add Budget Reports module with controller, routes, and seeding updates
    - Introduced `ReportsController` for budget reporting with export functionality.
    - Updated `budget.php` routes to include new report endpoints.
    - Added budget reports seeding in `ModuleSeeder`.
    - Corrected constant reference in `ReportsTrait` for module usage.
    
    Enhances reporting capabilities under the Budget module.
    
    Signed-off-by: Mureithi Maina <mureithi@razorinformatics.co.ke>

--- app/Traits/Controller/ReportsTrait.php
+++ app/Traits/Controller/ReportsTrait.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -60,17 +61,22 @@ trait ReportsTrait
                 }
                 $data = $service->parseReportXml($xmlResponse);
             } catch (ConnectionException $e) {
+                Log::error('Error Load Report :');
+                Log::error($e);
                 return view('snippets.errors')->with('message', 'cannot connect to the report server.');
             } catch (ErroredException $e) {
+                Log::error('Error Load Report :');
+                Log::error($e);
                 return view('snippets.errors')->with('message', $e->getMessage() ?? 'cannot retrieve report data.');
             } catch (Throwable|Exception $e) {
+                Log::error('Error Load Report :');
+                Log::error($e);
                 return view('snippets.errors')->with('message', 'cannot retrieve report data.');
             }
 
             return ($data->isEmpty())
                 ? view('snippets.errors')->with('message', 'Report has no data. Please check your report parameters and try again..')
                 : view('reports.table', compact('report', 'data'))->with('params', SSRSService::queryParams(collect($parameters)->put('_key', md5($report->Path))->toArray()));
-
         }
 
         try {
