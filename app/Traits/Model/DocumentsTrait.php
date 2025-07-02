2b2c20179542242576140183cdf01284e4e0a4c8
Mureithi Maina <mureithi@razorinformatics.co.ke>
2025-07-02 09:00:21 +0300

    Enhanced `DocumentsTrait` for model relationships
    - Updated `DocumentsTrait` with the `newDocument` method for document creation and management.
    - Improved `documents()` method to correctly handle morph relations.
    - Defined `committees` relationship in `Employee` model as a `BelongsToMany` relation for better structure and clarity.
    
    Signed-off-by: Mureithi Maina <mureithi@razorinformatics.co.ke>

--- app/Traits/Model/DocumentsTrait.php
+++ app/Traits/Model/DocumentsTrait.php
@@ -2,18 +2,34 @@
 
 namespace App\Traits\Model;
 
-use App\Models\Communication\SMS;
+use App\Enums\Core\ModulesEnum;
+use App\Exceptions\ErroredException;
+use App\Models\Auth\User;
+use App\Models\DMS\Document;
+use App\Services\DMS\DocumentService;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Http\UploadedFile;
 
 trait DocumentsTrait
 {
     public function documents(): MorphMany
     {
-        return $this->morphMany(SMS::class, 'related', "Related", "RelatedID", 'Id');
+        return $this->morphMany(__CLASS__, 'related', "Related", "RelatedID", 'Id');
     }
 
-    public function newDocument()
-     {
-         //todo fix this
-     }
+
+    /**
+     * @throws ErroredException
+     */
+    public function newDocument(ModulesEnum $module, UploadedFile $file, array|string $permissions, User $actor): Document
+    {
+        if (method_exists($this, 'getPrimaryKey')) {
+            throw new ErroredException("Implement UserActorTrait in model");
+        }
+
+        $Related = self::getPrimaryKey();
+        $RelatedId = $this->{$this->primaryKey};
+
+        return DocumentService::createInternal($module, $file, $actor, $permissions, $Related, $RelatedId)->document;
+    }
 }
