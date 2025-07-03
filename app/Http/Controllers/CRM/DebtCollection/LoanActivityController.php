cf7383d3ea4a70801736a150a52b81f182e6981b
Lemtukei-ruth <lemtukeiruth@gmail.com>
2025-07-03 13:57:42 +0100

    Bug ISP1412 Fix - Modified Activity tab to display the workflow of activities even when an activity gets deleted

--- app/Http/Controllers/CRM/DebtCollection/LoanActivityController.php
+++ app/Http/Controllers/CRM/DebtCollection/LoanActivityController.php
@@ -26,29 +26,47 @@ class LoanActivityController extends Controller
      */
     public function __invoke(Request $request, string $product_id): ActivityCollection|JsonResponse
     {
-        $product = DebtProduct::query()->where('AccountID', $product_id)->oldest('processDate')->first();
+        $product = DebtProduct::query()
+            ->where('AccountID', $product_id)
+            ->oldest('processDate')
+            ->first();
+
         if (!$product instanceof DebtProduct) {
             return $this->errored('Product not found, maybe closed.');
         }
+
         $this->authorize('view', $product);
 
         return new ActivityCollection(Activity::query()
             ->where(function (Builder $builder) use ($product) {
                 $builder->where(function (Builder $query) use ($product) {
                     $query->where('ActivityType', SMS::getPrimaryKey())
-                        ->whereIn('ActivityTypeID', SMS::query()->where('SourceID', $product->AccountID)->where('t_SMS.Source', DebtProduct::getPrimaryKey())->select('t_SMS.Id'));
+                        ->whereIn('ActivityTypeID', SMS::withTrashed()
+                            ->where('SourceID', $product->AccountID)
+                            ->where('t_SMS.Source', DebtProduct::getPrimaryKey())
+                            ->select('t_SMS.Id'));
                 })->orWhere(function (Builder $query) use ($product) {
                     $query->where('ActivityType', Email::getPrimaryKey())
-                        ->whereIn('ActivityTypeID', Email::query()->where('t_Emails.SourceID', $product->AccountID)->where('t_Emails.Source', DebtProduct::getPrimaryKey())->select('t_Emails.EmailID'));
+                        ->whereIn('ActivityTypeID', Email::withTrashed()
+                            ->where('t_Emails.SourceID', $product->AccountID)
+                            ->where('t_Emails.Source', DebtProduct::getPrimaryKey())
+                            ->select('t_Emails.EmailID'));
                 })->orWhere(function (Builder $query) use ($product) {
                     $query->where('ActivityType', Call::getPrimaryKey())
-                        ->whereIn('ActivityTypeID', Call::query()->where('t_Calls.SourceID', $product->AccountID)->where('t_Calls.Source', DebtProduct::getPrimaryKey())->select('t_Calls.CallID'));
+                        ->whereIn('ActivityTypeID', Call::withTrashed()
+                            ->where('t_Calls.SourceID', $product->AccountID)
+                            ->where('t_Calls.Source', DebtProduct::getPrimaryKey())
+                            ->select('t_Calls.CallID'));
                 })->orWhere(function (Builder $query) use ($product) {
                     $query->where('ActivityType', Task::getPrimaryKey())
-                        ->whereIn('ActivityTypeID', Task::query()->where('t_Tasks.SourceID', $product->AccountID)->where('t_Tasks.Source', DebtProduct::getPrimaryKey())->select('t_Tasks.TaskID'));
+                        ->whereIn('ActivityTypeID', Task::withTrashed()
+                            ->where('t_Tasks.SourceID', $product->AccountID)
+                            ->where('t_Tasks.Source', DebtProduct::getPrimaryKey())
+                            ->select('t_Tasks.TaskID'));
                 });
             })->orWhere(function (Builder $query) use ($product) {
-                $query->where('ActivityType', DebtProduct::getPrimaryKey())->where('ActivityTypeID', $product->AccountID);
+                $query->where('ActivityType', DebtProduct::getPrimaryKey())
+                    ->where('ActivityTypeID', $product->AccountID);
             })->latest('t_PartyActivities.CreatedOn')->simplePaginate(4));
     }
 }
