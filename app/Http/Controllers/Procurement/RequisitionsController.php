1eb5a6304ff8be093a45b49dea68617a0afe92b2
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-07-09 08:23:01 +0300

    Remove the dd in the code

--- app/Http/Controllers/Procurement/RequisitionsController.php
+++ app/Http/Controllers/Procurement/RequisitionsController.php
@@ -56,7 +56,6 @@ class RequisitionsController extends Controller
      */
     public function create()
     {
-         dd(session()->all());
         try {
             $details = $this->service->fetchRequisition();
             $branches = $this->service->fetchBranches();
