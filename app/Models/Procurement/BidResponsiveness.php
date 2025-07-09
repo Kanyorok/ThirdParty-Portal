1af4924df645b538ecf4a5bc7d20a519b7daea1a
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-07-09 08:21:58 +0300

    Remove the errors from the code

--- app/Models/Procurement/BidResponsiveness.php
+++ app/Models/Procurement/BidResponsiveness.php
@@ -16,9 +16,9 @@ class BidResponsiveness extends Model
 
     protected $table = 't_BidResponsiveness';
     protected $primaryKey = 'Id';
-    const string CREATED_AT = 'CreatedOn';
-    const string UPDATED_AT = 'ModifiedOn';
-    const string DELETED_AT = 'DeletedOn';
+    const CREATED_AT = 'CreatedOn';
+    const UPDATED_AT = 'ModifiedOn';
+    const DELETED_AT = 'DeletedOn';
 
     protected $fillable = [
         'TenderSupplierID',
