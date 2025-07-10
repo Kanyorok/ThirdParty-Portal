e4e048a9b2309a8d61b85c2b8f3fcde70646afc0
Mureithi Maina <mureithi@razorinformatics.co.ke>
2025-07-10 12:53:46 +0300

    Fix controller namespace and class name typos
    - Corrected the namespace for DocumentRecentController.
    - Fixed the typo in DocumentActionsController name.
    - Removed unused resource route for file-tags.
    
    Signed-off-by: Mureithi Maina <mureithi@razorinformatics.co.ke>

--- routes/dms.php
+++ routes/dms.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\DMS\DocumentRecentController;
-use App\Http\Controllers\DMS\Files\DocumentActionsConroller;
+
+use App\Http\Controllers\DMS\Files\DocumentActionsController;
 use App\Http\Controllers\DMS\Files\DocumentActivityController;
 use App\Http\Controllers\DMS\Files\DocumentController;
 use App\Http\Controllers\DMS\Files\DocumentPermissionController;
+use App\Http\Controllers\DMS\Files\DocumentRecentController;
 use App\Http\Controllers\DMS\Files\DocumentTagsController;
 use App\Http\Controllers\DMS\Files\DocumentUploadController;
 use App\Http\Controllers\DMS\Repo\RepositoryController;
@@ -18,12 +19,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::namespace('DMS')->prefix('dms')->group(function () {
-
     Route::get('recent', DocumentRecentController::class)->name('repo.recent');
     Route::get('bulk-upload', DocumentUploadController::class)->name('files.upload');
     Route::prefix('document/{document}')->group(function () {
         Route::get('activities', DocumentActivityController::class)->name('file.activities');
-        Route::get('preview', [DocumentActionsConroller::class, 'preview'])->name('file.preview');
+        Route::get('preview', [DocumentActionsController::class, 'preview'])->name('file.preview');
         Route::put('file-visibility', [DocumentPermissionController::class, 'visibility'])->name('file.visibility');
         Route::resource('document-tags', DocumentTagsController::class)->only(['create', 'store']);
         Route::resource('file-permissions', DocumentPermissionController::class)->only(['index', 'store', 'destroy']);
@@ -36,7 +36,6 @@ Route::namespace('DMS')->prefix('dms')->group(function () {
         Route::resource('files', DocumentController::class)->parameters(['files' => 'document'])->except('create');
     });
     Route::resource('repo', RepositoryController::class)->parameters(['repo' => 'repository']);
-    Route::resource('file-tags', TagController::class)->parameters(['file-tags' => 'DMSTag']);
 
     Route::prefix('file-tags/{d_m_s_tags}')->group(function () {
         Route::get('files', DocumentTagController::class)->name('file-tags.files');
@@ -52,5 +51,4 @@ Route::namespace('DMS')->prefix('dms')->group(function () {
         'index' => 'dms-reports.index',
         'show' => 'dms-reports.show'
     ]);
-
 });
