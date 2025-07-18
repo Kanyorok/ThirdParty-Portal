98e956d1c7c352965e92a846802a123fcaaa0a2c
kimxons <kitongameshack9@gmail.com>
2025-07-18 11:12:47 +0300

    validation messages for login request

--- app/Http/Requests/ThirdParty/LoginThirdPartyRequest.php
+++ app/Http/Requests/ThirdParty/LoginThirdPartyRequest.php
@@ -8,14 +8,34 @@ class LoginThirdPartyRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return true; //any dude can attempt to log in
     }
 
     public function rules(): array
     {
+        /**
+         * Validation rules for login request.
+         *
+         * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+         */
         return [
             'email' => 'required|email',
             'password' => 'required|string|min:6',
+        ];
+    }
+
+    /**
+     * Custom error messages for validation rules.
+     * Return as 422 (unprocessable entity) if validation fails.
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => __('auth.invalid_credentials'),
+            'email.email' => __('auth.invalid_credentials'),
+            'password.required' => __('auth.invalid_credentials'),
+            'password.min' => __('auth.invalid_credentials'),
         ];
     }
 }
