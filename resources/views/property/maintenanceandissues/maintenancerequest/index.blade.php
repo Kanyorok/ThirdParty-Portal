31468c91d3c62b258e099912f6611d8850e82298
perezkariuki <perezkariuki@gmail.com>
2025-07-21 12:57:46 +0300

    minor changes

--- resources/views/property/maintenanceandissues/maintenancerequest/index.blade.php
+++ resources/views/property/maintenanceandissues/maintenancerequest/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Item Sub Category')
+@section('title', 'Maintenance Requests')
 @section('styles')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 @endsection
