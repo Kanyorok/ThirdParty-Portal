d00c4233e84bea7813b267a5ffc84c868a5bdd07
Robert Kanyoro <rmjesusaddict@gmail.com>
2025-07-22 10:09:34 +0300

    Hotfix the pricing for the field PriceID

--- app/Services/Inventory/PriceManagementService.php
+++ app/Services/Inventory/PriceManagementService.php
@@ -43,7 +43,6 @@ class PriceManagementService
         $pricing->CreatedOn = Carbon::now();
         $pricing->ModifiedBy = Auth::id();
         $pricing->ModifiedOn = Carbon::now();
-        $pricing->save();
 
         $pricing->PriceID = 'PR-' . str_pad($pricing->Id, 5, '0', STR_PAD_LEFT);
         $pricing->save();
