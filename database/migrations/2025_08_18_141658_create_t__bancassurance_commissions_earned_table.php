62b9e4ceaa5baed2629b13dbc62e6a2a3f36cabc
Danson Njenga <Danson.Njenga@CRAFTSILICON.LOCAL>
2025-08-18 16:24:40 +0300

    changes on insurance

---
+++ database/migrations/2025_08_18_141658_create_t__bancassurance_commissions_earned_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_BancassuranceCommissionsEarned', function (Blueprint $table) {
+            $table->id('Id');
+            $table->string('PolicyId')->constrained('t_BancassurancePolicies', 'Id');
+            $table->foreignId('ReferralId')->constrained('t_BancassuranceReferrals', 'Id');
+            $table->foreignId('CommissionRuleId')->constrained('t_BancassuranceCommissionRules', 'Id');
+            $table->float('CommissionRate');
+            $table->float('FixedAmount');
+            $table->foreignId('AppliesTo')->constrained('t_CodeDetails', 'ID');
+            $table->boolean('IsActive');
+            $table->foreignId('CreatedBy')->constrained('t_Users', 'Id');
+            $table->dateTime('CreatedOn');
+            $table->foreignId('ModifiedBy')->constrained('t_Users', 'Id');
+            $table->dateTime('ModifiedOn');
+            $table->foreignId('DeletedBy')->nullable()->constrained('t_Users', 'Id');
+            $table->dateTime('DeletedOn')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_BancassuranceCommissionsEarned');
+    }
+};
