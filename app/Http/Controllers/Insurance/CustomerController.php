b91d164b7a666ba7b7a386dc8b1d30066b2b1f20
perezkariuki <perezkariuki@gmail.com>
2025-08-20 15:23:52 +0300

    update on customer

--- app/Http/Controllers/Insurance/CustomerController.php
+++ app/Http/Controllers/Insurance/CustomerController.php
@@ -74,6 +74,7 @@ class CustomerController extends Controller
 
     public function check()
     {
+        //check
        $customers = BancassuranceCustomer::all();
 
         return view('bancassurance.customers.check', compact('customers'));
