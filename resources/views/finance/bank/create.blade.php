84c5118cdbc64376d23d5d21fd84e89e3d380405
Martin Njoroge <martin.njoroge@CRAFTSILICON.LOCAL>
2025-09-17 11:09:48 +0300

    Feature for BankManagement

--- resources/views/finance/bank/create.blade.php
+++ resources/views/finance/bank/create.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
+@section('title', 'Create Bank')
 @section('content')
 <div class="container">
-    <h1>Create Bank</h1>
-    <form action="{{ route('finance.bank.store') }}" method="POST" class="mt-3">
-        @csrf
-        @include('finance.bank._form', ['bank' => null])
-        <div class="mt-4">
-            <button class="btn btn-success">Save</button>
-            <a href="{{ route('finance.bank.index') }}" class="btn btn-secondary">Cancel</a>
-        </div>
-    </form>
+    <div class="card p-3">
+        <form action="{{ route('finance.bank.store') }}" method="POST" class="mt-3">
+            @csrf
+            @include('finance.bank._form', ['bank' => null])
+            <div class="mt-4">
+                <button class="btn btn-success">Save</button>
+                <a href="{{ route('finance.bank.index') }}" class="btn btn-secondary">Cancel</a>
+            </div>
+        </form>
+    </div>
+
 </div>
 @endsection
