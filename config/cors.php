821c029057fb5062e8439465f166de268944184a
Kanyorok <rmjesusaddict@gmail.com>
2025-09-18 16:00:28 +0300

    Fix the Cors policy

--- config/cors.php
+++ config/cors.php
@@ -8,7 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-    'https://your-frontend-domain.com',
+        'http://172.16.2.16:3001',
     ],
 
     'allowed_origins_patterns' => [],
