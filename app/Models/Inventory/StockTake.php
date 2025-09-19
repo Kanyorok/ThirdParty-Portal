38b6a03ae8d0a0e520fe1e82794b9c9e6aa664b2
perezkariuki <perezkariuki@gmail.com>
2025-09-19 11:02:44 +0300

    relationship update

--- app/Models/Inventory/StockTake.php
+++ app/Models/Inventory/StockTake.php
@@ -8,7 +8,6 @@ use App\Models\Core\Branch;
 use App\Traits\Model\UserActorTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Inventory\store;
 
 
 class StockTake extends Model
@@ -40,12 +39,12 @@ class StockTake extends Model
 
     public function branch()
     {
-        return $this->belongsTo(Store::class, 'BranchId', 'Id');
+        return $this->belongsTo(Branch::class, 'BranchId', 'Id');
     }
 
     public function store()
     {
-        return $this->belongsTo(Branch::class, 'StoreId', 'Id');
+        return $this->belongsTo(Store::class, 'StoreId', 'Id');
     }
 
     public function createdby()
