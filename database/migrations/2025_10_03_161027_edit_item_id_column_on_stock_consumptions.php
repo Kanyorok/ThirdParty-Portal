5e96edd2cc1682aeaea363c44dfc370013f2b1dd
Martin Njoroge <martinnjoroge0028@gmail.com>
2025-10-04 08:56:28 +0300

    Update 2025_10_03_161027_edit_item_id_column_on_stock_consumptions.php
    Eddit migration

--- database/migrations/2025_10_03_161027_edit_item_id_column_on_stock_consumptions.php
+++ database/migrations/2025_10_03_161027_edit_item_id_column_on_stock_consumptions.php
@@ -10,27 +10,27 @@ return new class extends Migration
     {
         Schema::table('t_StockConsumptions', function (Blueprint $table) {
             // Drop old foreign key
-            $table->dropForeign(['ItemID']);
+            //$table->dropForeign(['ItemID']);
 
             // Add new foreign key to t_StockItems
-            $table->foreign('ItemID')
-                  ->references('Id')
-                  ->on('t_StockItems')
-                  ->nullOnDelete(); // optional: set null if stock item deleted
-        });
+        //     $table->foreign('ItemID')
+        //           ->references('Id')
+        //           ->on('t_StockItems')
+        //           ->nullOnDelete(); // optional: set null if stock item deleted
+        // });
     }
 
     public function down(): void
     {
         Schema::table('t_StockConsumptions', function (Blueprint $table) {
             // Rollback: drop new foreign key
-            $table->dropForeign(['ItemID']);
+            // $table->dropForeign(['ItemID']);
 
             // Restore old foreign key to t_Items
-            $table->foreign('ItemID')
-                  ->references('Id')
-                  ->on('t_Items')
-                  ->nullOnDelete();
-        });
+        //     $table->foreign('ItemID')
+        //           ->references('Id')
+        //           ->on('t_Items')
+        //           ->nullOnDelete();
+        // });
     }
 };
