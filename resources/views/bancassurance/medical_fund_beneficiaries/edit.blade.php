379be826ab138ae96b19bb92851fffbd23e5af60
perezkariuki <perezkariuki@gmail.com>
2025-10-24 08:37:03 +0300

    Medical fund update

--- resources/views/bancassurance/medical_fund_beneficiaries/edit.blade.php
+++ resources/views/bancassurance/medical_fund_beneficiaries/edit.blade.php
@@ -14,7 +14,7 @@
     <div class="card">
         <div class="card-body">
             {{-- 🔁 Shallow member route for update --}}
-            <form action="{{ route('bancassurance.beneficiaries.update', $beneficiary->ID) }}" method="POST">
+            <form action="{{ route('bancassurance.beneficiaries.update', $beneficiary->Id) }}" method="POST">
                 @csrf
                 @method('PUT')
 
@@ -74,7 +74,7 @@
                 <div class="mt-3 d-flex gap-2">
                     <button class="btn btn-primary">Update</button>
                     {{-- 📚 Collection route remains nested (by fund) --}}
-                    <a href="{{ route('bancassurance.medicalfunds.beneficiaries.index', ['medical_fund' => $medical_fund->ID]) }}"
+                    <a href="{{ route('bancassurance.medicalfunds.beneficiaries.index', ['medical_fund' => $medical_fund->Id]) }}"
                        class="btn btn-outline-secondary">Back</a>
                 </div>
             </form>
