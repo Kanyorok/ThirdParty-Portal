4ff8c94e89b18f16b3fb3f20be2748269acf8413
Mureithi Maina <mureithi@razorinformatics.co.ke>
2025-10-31 16:09:24 +0300

    **feat(ITRACK-999): integrate iTrack vehicle playback functionality**
    - Added GPS coordinates model (`GPSCoordinate`) with morphable relationships for flexibility.
    - Introduced `FleetDayPlaybackCommand` for daily playback data retrieval from iTrack.
    - Created migrations for `GPSCoordinates` table and added `TrackerNo` column to `FleetVehicle` table.
    - Updated `FleetVehicle` model to include GPS coordinate relationships.
    
    Signed-off-by: Mureithi Maina <mureithi@razorinformatics.co.ke>

---
+++ app/Models/Core/GPSCoordinate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Core;
+
+use App\Traits\Model\UserActorTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class GPSCoordinate extends Model
+{
+    use UserActorTrait;
+
+    const string CREATED_AT = 'CreatedOn';
+    const string UPDATED_AT = 'ModifiedOn';
+    protected $table = 't_GPSCoordinates';
+    protected $primaryKey = 'Id';
+
+    protected $fillable = [
+        "Source", "SourceID", "Latitude", "Longitude", "Extra",
+        "CreatedBy", "CreatedOn", "ModifiedBy", "ModifiedOn"
+    ];
+
+    protected $casts = [
+        'Latitude' => 'float',
+        'Longitude' => 'float',
+        'Extra' => 'array',
+    ];
+
+
+    public static function getPrimaryKey(): string
+    {
+        return 'GPSCoordinateID';
+    }
+
+    public function source(): MorphTo
+    {
+        return $this->morphTo(__FUNCTION__, "Source", "SourceID", 'Id');
+    }
+}
