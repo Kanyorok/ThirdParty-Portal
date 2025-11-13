0fc967ef57340b1dd818a11f6d888481244ed1d4
joythwagi <james.muhuthia@CRAFTSILICON.LOCAL>
2025-11-13 16:44:01 +0300

    Added soft delete

--- app/Models/Fleet/FuelType.php
+++ app/Models/Fleet/FuelType.php
@@ -3,18 +3,35 @@
 namespace App\Models\Fleet;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FuelType extends Model
 {
+    use SoftDeletes;
+
     protected $table = 't_FuelTypes';
     protected $primaryKey = 'Id';
     public $timestamps = false;
-    protected $fillable = ['FuelName', 'IsActive', 'FuelTypeCode', 'Description', 'CreatedBy', 'CreatedOn', 'ModifiedBy', 'ModifiedOn', 'DeletedBy', 'DeletedOn'];
 
+    // Tell Laravel to use your custom DeletedOn column
+    const DELETED_AT = 'DeletedOn';
+    protected $dates = ['DeletedOn'];
 
- public function vehicles()
+    protected $fillable = [
+        'FuelName',
+        'IsActive',
+        'FuelTypeCode',
+        'Description',
+        'CreatedBy',
+        'CreatedOn',
+        'ModifiedBy',
+        'ModifiedOn',
+        'DeletedBy',
+        'DeletedOn'
+    ];
+
+    public function vehicles()
     {
         return $this->hasMany(FleetVehicle::class, 'FuelType', 'Id');
     }
-
 }
