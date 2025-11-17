e97ad71643a34396a987892d4d3e25757fcd87f5
Lemtukei-ruth <lemtukeiruth@gmail.com>
2025-11-17 09:49:25 +0300

    Configured soft deletes on FuelType

--- app/Models/Fleet/FuelType.php
+++ app/Models/Fleet/FuelType.php
@@ -4,18 +4,20 @@ namespace App\Models\Fleet;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Model\UserActorTrait;
+
 
 class FuelType extends Model
 {
-    use SoftDeletes;
+    use UserActorTrait, SoftDeletes;
 
     protected $table = 't_FuelTypes';
     protected $primaryKey = 'Id';
     public $timestamps = false;
 
-    // Tell Laravel to use your custom DeletedOn column
+    const CREATED_AT = 'CreatedOn';
+    const UPDATED_AT = 'ModifiedOn';
     const DELETED_AT = 'DeletedOn';
-    protected $dates = ['DeletedOn'];
 
     protected $fillable = [
         'FuelName',
