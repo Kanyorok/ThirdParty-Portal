c386f8f3e4e0ae52824573d7e9bba5a7bae96835
Martin Njoroge <martinnjoroge0028@gmail.com>
2025-11-17 11:48:05 +0300

    Handle null values in item type descriptions

--- resources/views/inventory/itemmaster/itemtype/index.blade.php
+++ resources/views/inventory/itemmaster/itemtype/index.blade.php
@@ -39,7 +39,7 @@
                                     <div class="d-flex gap-1">
                                         {{-- View --}}
                                         <button type="button" class="btn btn-sm btn-info"
-                                            onclick="showItemModal('{{ $itemtype->type->Description }}', '{{ $itemtype->StockTracked ? 'Yes' : 'No' }}', '{{ $itemtype->RequiresTagging ? 'Yes' : 'No' }}', '{{ $itemtype->Active ? 'Yes' : 'No' }}')"
+                                            onclick="showItemModal('{{ $itemtype->type->Description ?? 'N/A' }}', '{{ $itemtype->StockTracked ? 'Yes' : 'No' }}', '{{ $itemtype->RequiresTagging ? 'Yes' : 'No' }}', '{{ $itemtype->Active ? 'Yes' : 'No' }}')"
                                             title="View">
                                             <i class="fas fa-eye"></i>
                                         </button>
@@ -109,7 +109,7 @@
                         <select class="form-select" id="editTypeName" name="TypeName" required>
                             <option value="">Select Item Type</option>
                             @foreach($itmTypes as $itmType)
-                                <option value="{{ $itmType->ID }}">{{ $itmType->Description }}</option>
+                                <option value="{{ $itmType->ID }}">{{ $itmType->Description ?? 'N/A' }}</option>
                             @endforeach
                         </select>
                     </div>
