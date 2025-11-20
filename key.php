88dde4e9c4b2017cdbb7c71d6dbdbb2aebb3eb47
Waruguru Aurelia Mundia <awmundia@kabarak.ac.ke>
2025-11-20 10:39:32 +0300

    Corrected key.php to match the license file

--- key.php
+++ key.php
@@ -1,8 +1,9 @@
 <?php
+#in the key.php file
 if (!function_exists("sodium_crypto_sign_keypair")) {
     fwrite(STDERR, "Sodium missing\n");
     exit(1);
 }
 $kp = sodium_crypto_sign_keypair();
+echo "PRIVATE_KEY_BASE64=" . base64_encode(sodium_crypto_sign_secretkey($kp)) . PHP_EOL;
 echo "PUBLIC_KEY_BASE64=" . base64_encode(sodium_crypto_sign_publickey($kp)) . PHP_EOL;
-
