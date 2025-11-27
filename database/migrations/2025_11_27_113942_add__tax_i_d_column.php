57b865b6df58b88126dc3af43871259de5bd3af2
martinjoz <martinnjoroge0028@gmail.com>
2025-11-27 14:36:28 +0300

    Done Tax impl

--- database/migrations/2025_11_27_113942_add__tax_i_d_column.php
+++ database/migrations/2025_11_27_113942_add__tax_i_d_column.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::table('t_FinanceInvoices', function (Blueprint $table) {
             //Add foreign ID to the TaxID column
             $table->foreignId('TaxID')->nullable()->constrained('t_FinanceTaxRuleConfiguration', 'Id');
+            $table->decimal('TaxAmount', 10, 2)->default(0);
+            //Tax Percentage
+            $table->decimal('TaxPercentage', 10, 4)->default(0);
+            $table->decimal('InvoiceAmount', 10, 2)->default(0);
         });
     }
 
@@ -27,6 +31,9 @@ return new class extends Migration
             $table->dropForeign(['TaxID']);
             //Drop the column
             $table->dropColumn('TaxID');
+            $table->dropColumn('TaxAmount');
+            $table->dropColumn('TaxPercentage');
+            $table->dropColumn('InvoiceAmount');
         });
     }
 };
