98da67b8107d75e3c9f6e2c6ed34ba738651da50
Mureithi Maina <mureithi@razorinformatics.co.ke>
2025-12-01 11:54:41 +0300

    **fix(feature/thirdparty-redesign): update authentication middleware and dashboard controller**
    - Refactored `Authenticate` middleware to enhance branch-role validation logic.
    - Improved invalid session handling with detailed activity logging and update of session invalidation process.
    - Updated `DashboardController` to replace deprecated `$actorId` logic with direct user ID usage.
    
    Signed-off-by: Mureithi Maina <mureithi@razorinformatics.co.ke>

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,27 +2,84 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Auth\ModelRole;
+use App\Models\Auth\Role;
+use App\Models\Auth\User;
+use App\Models\Core\Branch;
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
-    protected function redirectTo(Request $request): ?string
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param string ...$guards
+     * @return mixed
+     *
+     * @throws AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards): mixed
     {
-        return $request->expectsJson() ? null : route('login');
-    }
-
-    protected function unauthenticated($request, array $guards)
-    {
-        if ($request->expectsJson()) {
-            throw new AuthenticationException(
-                'Unauthenticated.',
-                $guards,
-                $this->redirectTo($request)
-            );
+        $this->authenticate($request, $guards);
+        $actor = $request->user();
+        $branch = $actor->branch;
+        if (!$actor instanceof User || !$branch instanceof Branch) {
+            $this->unauthenticated($request, $guards, $actor);
         }
 
-        parent::unauthenticated($request, $guards);
+        if (session()?->has('branch_id')) {
+            $modelRole = ModelRole::query()->where('model_id', $actor->Id)
+                ->where('model_type', User::getPrimaryKey())
+                ->where('BranchId', $branch->Id)->with('role')->first();
+            $role = $modelRole?->role;
+            if (!$modelRole instanceof ModelRole || !$role instanceof Role) {
+                $this->unauthenticated($request, $guards);
+            }
+
+            session([
+                'LoginBranchId' => $branch->Id,
+                'LoginBranchName' => $branch->Name,
+                'LoginRoleName' => $role->name,
+                'login_at' => $actor->last_login_at,
+            ]);
+        }
+
+        return $next($request);
     }
+
+    protected function unauthenticated($request, array $guards, User $user = null): void
+    {
+        if ($user instanceof User) {
+            activity()
+                ->causedBy($user)
+                ->performedOn($user)
+                ->event('authentication')
+                ->log('Invalid session from ' . $request->getClientIp());
+            $user->update([
+                'BranchId' => null,
+            ]);
+        }
+
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        throw new AuthenticationException(
+            'Unauthenticated.',
+            $guards,
+            $request->expectsJson() ? null : $this->redirectTo($request),
+        );
+    }
+
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() || $request->json() ? null : route('login');
+    }
+
 }
