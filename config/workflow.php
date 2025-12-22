c23a99bfd1c5987c7c02ba5c5c8241eb8e27b6c9
raphael.maina <raphmaina7@gmail.com>
2025-12-22 08:08:00 +0300

    fixed config/worflow errors

--- config/workflow.php
+++ config/workflow.php
@@ -69,10 +69,7 @@ return [
         'Submitted for Approval' => \App\Enums\Core\ApprovalEnum::Submitted->value,  // 's'
     ],
 
-    'Approved' => \App\Enums\TenderApprovalStatusEnum::APPROVED->value, // Will now be 'Ap'
-    'Rejected' => \App\Enums\TenderApprovalStatusEnum::REJECTED->value, // Will now be 'R'
-    'Pending'  => \App\Enums\TenderApprovalStatusEnum::PENDING->value,  // Will now be 'P'
-],
+
      'RFQId' => [
         'Approved' => WorkflowStatus::Approved->value,  // 'A'
         'Rejected' => WorkflowStatus::Rejected->value,  // 'R'
