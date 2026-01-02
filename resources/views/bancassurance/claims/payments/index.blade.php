5e504fb6e6ab6e26604d6ac059d96cbec807080f
perezkariuki <perezkariuki@gmail.com>
2026-01-02 13:10:39 +0300

    update changes on server error bug fixes

--- resources/views/bancassurance/claims/payments/index.blade.php
+++ resources/views/bancassurance/claims/payments/index.blade.php
@@ -75,12 +75,8 @@
                             <td>{{ \Carbon\Carbon::parse($pay->PaymentDate)->format('d M Y') }}</td>                           <td>{{ $pay->PaymentReference ?? '-'}}</td>
                         </tr>
                         @empty
-                        <tr>
-                            <td colspan="7" class="text-center text-muted py-4">
                                 <i class="bi bi-inbox fs-4 d-block mb-2"></i>
                                 No payments found.
-                            </td>
-                        </tr>
                         @endforelse
                     </tbody>
                 </table>
