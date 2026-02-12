0e77c21c0ba1f23551efd4e3372b6daa3e0ec9e7
Kanyorok <rmjesusaddict@gmail.com>
2026-02-12 17:46:03 +0300

    fix: PHP CS Fixer - fix indentation and missing newline in property routes

--- routes/integrations/property.php
+++ routes/integrations/property.php
@@ -6,24 +6,23 @@ use App\Http\Controllers\API\Property\PropertyMaintenanceRequestController;
 use App\Http\Controllers\API\Property\PropertyViewController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::prefix('property')->middleware(['auth.thirdparty'])->group(function () {
-// Property View Routes
-Route::get('rentable-properties', [PropertyViewController::class, 'index']);
+    // Property View Routes
+    Route::get('rentable-properties', [PropertyViewController::class, 'index']);
 
-// Property Lease Routes
-Route::get('leases/tenant', [PropertyLeaseController::class, 'index']);
-Route::get('leases/tenant/show', [PropertyLeaseController::class, 'show']);
+    // Property Lease Routes
+    Route::get('leases/tenant', [PropertyLeaseController::class, 'index']);
+    Route::get('leases/tenant/show', [PropertyLeaseController::class, 'show']);
 
-// Property Invoice Routes
-Route::get('invoices/tenant', [PropertyInvoiceController::class, 'index']);
-Route::get('invoices/tenant/show', [PropertyInvoiceController::class, 'show']);
+    // Property Invoice Routes
+    Route::get('invoices/tenant', [PropertyInvoiceController::class, 'index']);
+    Route::get('invoices/tenant/show', [PropertyInvoiceController::class, 'show']);
 
 
-// Maintenance Request Routes
-Route::get('maintenancerequest', [PropertyMaintenanceRequestController::class, 'index']);
-Route::post('maintenancerequest', [PropertyMaintenanceRequestController::class, 'store']);
-Route::get('maintenancerequest/{id}', [PropertyMaintenanceRequestController::class, 'show']);
-Route::delete('maintenancerequest/{id}', [PropertyMaintenanceRequestController::class, 'destroy']);
+    // Maintenance Request Routes
+    Route::get('maintenancerequest', [PropertyMaintenanceRequestController::class, 'index']);
+    Route::post('maintenancerequest', [PropertyMaintenanceRequestController::class, 'store']);
+    Route::get('maintenancerequest/{id}', [PropertyMaintenanceRequestController::class, 'show']);
+    Route::delete('maintenancerequest/{id}', [PropertyMaintenanceRequestController::class, 'destroy']);
 
 });
