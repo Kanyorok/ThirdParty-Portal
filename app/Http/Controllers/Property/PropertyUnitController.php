0ecb6c4479732504d34b44be3d221d0a224343a0
perezkariuki <perezkariuki@gmail.com>
2026-02-23 14:44:23 +0300

    Unit size minimum is 1

--- app/Http/Controllers/Property/PropertyUnitController.php
+++ app/Http/Controllers/Property/PropertyUnitController.php
@@ -108,7 +108,7 @@ class PropertyUnitController extends Controller
             'BlockID' => 'required|exists:t_PropertyBlock,Id',
             'FloorID' => 'required|exists:t_PropertyFloor,Id',
             'UnitCode' => 'required|string|max:50',
-            'UnitSize' => 'required|integer|min:0',
+            'UnitSize' => 'required|integer|min:1',
             'IsRentable' => 'required|boolean',
             'CurrentStatus' => 'required|boolean',
             'Remarks' => 'nullable|string|max:50',
