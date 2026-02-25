bc383c4ea5612a4335ba062116aa7178dfb55129
Kanyorok <rmjesusaddict@gmail.com>
2026-02-25 09:19:44 +0300

    fix: Update invalid user foreign key references in prequalification applications to valid user IDs.

--- database/migrations/2026_02_19_161000_fix_prequalification_applications_createdby_fk.php
+++ database/migrations/2026_02_19_161000_fix_prequalification_applications_createdby_fk.php
@@ -20,7 +20,33 @@ return new class extends Migration {
             $table->dropForeign(['CreatedBy']);
             $table->dropForeign(['ModifiedBy']);
             $table->dropForeign(['DeletedBy']);
+        });
 
+        // Fix existing data that doesn't map to t_Users
+        $firstUserId = \Illuminate\Support\Facades\DB::table('t_Users')->orderBy('Id')->value('Id');
+        if ($firstUserId) {
+            \Illuminate\Support\Facades\DB::table('t_SupplierPrequalificationApplications')
+                ->whereNotIn('CreatedBy', function($query) {
+                    $query->select('Id')->from('t_Users');
+                })
+                ->update(['CreatedBy' => $firstUserId]);
+
+            \Illuminate\Support\Facades\DB::table('t_SupplierPrequalificationApplications')
+                ->whereNotNull('ModifiedBy')
+                ->whereNotIn('ModifiedBy', function($query) {
+                    $query->select('Id')->from('t_Users');
+                })
+                ->update(['ModifiedBy' => $firstUserId]);
+
+            \Illuminate\Support\Facades\DB::table('t_SupplierPrequalificationApplications')
+                ->whereNotNull('DeletedBy')
+                ->whereNotIn('DeletedBy', function($query) {
+                    $query->select('Id')->from('t_Users');
+                })
+                ->update(['DeletedBy' => $firstUserId]);
+        }
+
+        Schema::table('t_SupplierPrequalificationApplications', function (Blueprint $table) {
             // Re-add pointing at t_Users.Id
             $table->foreign('CreatedBy')->references('Id')->on('t_Users');
             $table->foreign('ModifiedBy')->references('Id')->on('t_Users')->nullable();
