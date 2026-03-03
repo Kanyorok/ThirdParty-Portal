f4b8cde63d111018ce89974ba205eb5b8ff78775
kimxons <kitongameshack9@gmail.com>
2026-03-03 10:31:46 +0300

    code alignment with dev

--- config/workflow.php
+++ config/workflow.php
@@ -96,12 +96,6 @@ return [
         'Rejected' => \App\Enums\TenderApprovalStatusEnum::REJECTED->value, // Will now be 'R'
         'Pending' => \App\Enums\TenderApprovalStatusEnum::PENDING->value,  // Will now be 'P'
     ],
-    'LeaseTerminationId' => [
-        'Approved' => \App\Enums\Core\ApprovalEnum::Approved->value,  // 'a'
-        'Rejected' => \App\Enums\Core\ApprovalEnum::Rejected->value,  // 'r'
-        'Pending' => \App\Enums\Core\ApprovalEnum::Pending->value,    // 'p'
-        'Submitted for Approval' => \App\Enums\Core\ApprovalEnum::Submitted->value,  // 's'
-    ],
 
     'ScheduleRenewalId' => [
         'Approved' => \App\Enums\Core\ApprovalEnum::Approved->value,  // 'a'
